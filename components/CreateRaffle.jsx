ab0c90ffd12108dbd7b02a08b24146f0bbc38742
Jeewoo Lee <44099418+jeewoo-lee@users.noreply.github.com>
2022-07-05 18:27:31 +0900

    creating contract functions

--- components/CreateRaffle.jsx
+++ components/CreateRaffle.jsx
@@ -1,5 +1,5 @@
 import { useMoralis, useWeb3Contract } from "react-moralis"
-import { abi, factoryAddress } from "../constants"
+import { factoryABI, factoryAddress } from "../constants"
 import { useEffect, useState } from "react"
 import { ethers } from "ethers"
 import { useNotification } from "web3uikit"
@@ -7,8 +7,44 @@ import { useNotification } from "web3uikit"
 export default function CreateRaffle() {
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
   const chainId = parseInt(chainIdHex)
-  const raffleContract = chainId in factoryAddress ? factoryAddress[chainId][0] : null
-  const [itemPrice, setitemPrice] = useState("0")
-  const [totalDeposited, settotalDeposited] = useState("0")
-  const [recentWinNum, setrecentWinNum] = useState("0")
+  const raffleFactory = chainId in factoryAddress ? factoryAddress[chainId][0] : null
+  const [raffles, setRaffles] = useState("0")
+  const [minInput, setMinInput] = useState("0")
+  const [raffleId, setRaffleId] = useState("0")
+
+  const formatUnits = ethers.utils.formatUnits
+  const dispatch = useNotification()
+
+  let itemPrice, interval, inputId
+
+  const {
+    runContractFunction: createRaffle,
+    isFetching,
+    isLoading,
+  } = useWeb3Contract({
+    abi: factoryABI,
+    contractAddress: raffleFactory,
+    functionName: "createRaffle",
+    params: { _itemPrice: itemPrice, _interval: interval },
+  })
+  const { runContractFunction: getRaffleId } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress, // specify the networkId
+    functionName: "raffleId",
+    params: {},
+  })
+
+  const { runContractFunction: getRaffles } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress, // specify the networkId
+    functionName: "raffles",
+    params: { id: inputId },
+  })
+
+  const { runContractFunction: getMinInput } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress, // specify the networkId
+    functionName: "minInput",
+    params: {},
+  })
 }
